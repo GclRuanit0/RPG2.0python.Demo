4663b053b96159033ec63523026ff858965d08d5
Endrio <ruanito.mit@gmail.com>
2025-11-30 21:24:15 -0300

    Update historia.py
    25 mana

--- historia.py
+++ historia.py
@@ -13,7 +13,7 @@ def introducao():
     print_lento("\nSua cabeça explode de dor. Mil martelos batem dentro de você.")
     print_lento("\nVocê acorda em uma cela fria e úmida, preso por correntes de gelo.")
     print_lento("\nLuz fraca entra pelas frestas de uma porta enferrujada.")
-    print_lento("\nCom esforço, você quebra as correntes e rasteja até a porta.")
+    print_lento("\nCom esforço,gastando 25 de mana, você quebra as correntes e rasteja até a porta.")
     print_lento("\nAo empurrá-la, um rangido ecoa pelo corredor vazio...")
     print_lento("\nVocê não está sozinho.")
     print_lento("\nÀ frente, um som estranho rompe o silêncio.")
